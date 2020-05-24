fda1528a88706d1970122933f3675f50ef4e1dab
Pierre Gorel <pgorel@snolab.ca>
2020-05-23 23:02:36 -0400

    First succes at introducing I:E in the get_table

--- python/get_tables.py
+++ python/get_tables.py
@@ -26,11 +26,13 @@ def get_table(df):
       # SIM measurements
       ('simulator $V_{tidal}$ [ml]', f'${float(row["simulator_volume_ml"]):.0f}$'),
       ('simulator $P_{plateau}$ [cmH2O]', f'${float(row["simulator_plateau"]):.0f}$'),
+      ('simulator I:E', f'${float(row["simulator_iovere"]):.0f}$'),
       # MVM measurements
       ('measured $V_{tidal}$ [ml]', f'${float(row["mean_volume_ml"]):.0f} \pm {float(row["rms_volume_ml"]):.0f}$'),
       ('measured $P_{plateau}$ [cmH2O]', f'${float(row["mean_plateau"]):.0f} \pm {float(row["rms_plateau"]):.0f}$'),
       ('measured $P_{peak}$ [cmH2O]', f'${float(row["mean_peak"]):.0f} \pm {float(row["rms_peak"]):.0f}$'),
       ('measured PEEP [cmH2O]', f'${float(row["mean_peep"]):.0f} \pm {float(row["rms_peep"]):.0f}$'),
+      ('measured I:E', f'${float(row["mean_iovere"]):.0f}$'),
     ]
 
     if i == 0:
@@ -106,7 +108,7 @@ def process_files(files, output_dir, save_h5=False):
 #   ('set $P_{insp}$', '$P_{peak}$ from MVM', '[$cmH_{2}O$]', 'Pinspiratia', 'mean_peak', 'rms_peak', 'max_peak', 'min_peak'),
     ('target $V_{tidal}$', '$V_{tidal}$ from MVM', '[ml]', 'Tidal Volume', 'mean_volume_ml', 'rms_volume_ml', 'max_volume_ml', 'min_volume_ml'),
     ('$V_{tidal}$ from lung simulator', '$V_{tidal}$ from MVM', '[ml]', 'simulator_volume_ml', 'mean_volume_ml', 'rms_volume_ml', 'max_volume_ml', 'min_volume_ml'),
-    # ('I:E from lung simulator', 'I:E from MVM'
+    ('I:E from lung simulator', 'I:E from MVM', '[to be removed]', 'simulator_iovere', 'mean_iovere', 'rms_iovere', 'max_iovere', 'min_iovere'),
     # ('set I:E', 'I:E from MVM'
     # ('breath rate from lung simulator', 'breath rate from MVM'
     # ('set breath rate', 'breath rate from MVM'
@@ -116,12 +118,14 @@ def process_files(files, output_dir, save_h5=False):
   maximum_bias_error = {
     'mean_peep' : MVM.maximum_bias_error_peep,
     'mean_plateau' : MVM.maximum_bias_error_pinsp,
-    'mean_volume_ml' : MVM.maximum_bias_error_volume
+    'mean_volume_ml' : MVM.maximum_bias_error_volume,
+    'mean_iovere' : MVM.maximum_bias_error_volume
   }
   maximum_linearity_error = {
     'mean_peep' : MVM.maximum_linearity_error_peep,
     'mean_plateau' : MVM.maximum_linearity_error_pinsp,
-    'mean_volume_ml' : MVM.maximum_linearity_error_volume
+    'mean_volume_ml' : MVM.maximum_linearity_error_volume,
+    'mean_iovere' : MVM.maximum_linearity_error_volume
   }
 
   line = lmfit.models.LinearModel()
